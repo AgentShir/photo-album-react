de8193453af31d3580b98ea6ce1cd6e14a07dc83
Shireen Garcia <garciasal@gmail.com>
2017-09-12 15:54:26 -0700

    Album routes work

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
 import '../styles/App.css';
 
 class BaseLayout extends Component {
   render() {
     return(
-      <h1>Hello from the Base Layout!</h1>
+      <div className="wrapper">
+        <div className="container">
+          <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+            <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <NavLink className="navbar-brand" to="#">Photo Album</NavLink>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav">
+              <NavLink className="nav-item nav-link active" to="/home">Home <span className="sr-only">(current)</span></NavLink>
+              <NavLink className="nav-item nav-link" to="/album1">Album 1</NavLink>
+              <NavLink className="nav-item nav-link" to="album2">Album 2</NavLink>
+              <NavLink className="nav-item nav-link" to="album3">Album 3</NavLink>
+              <NavLink className="nav-item nav-link" to="album4">Album 4</NavLink>
+              <NavLink className="nav-item nav-link" to="album5">Album 5</NavLink>
+              <NavLink className="nav-item nav-link" to="album6">Album 6</NavLink>
+              </div>
+            </div>
+          </nav>
+          {this.props.children}
+        </div>
+      </div>
     )
   }
 }
